2ed22d7b0888372dfcf461241dcf4d5f4c0bfaaa
None <33761787+100518832@users.noreply.github.com>
2018-02-27 12:41:50 -0500

    code clean up

--- ww_flask.py
+++ ww_flask.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 CORS(app)
 import random
 
-#card_points = {'good':{'villager':1, 'witch':4, 'pacifist':-1, 'p.i':3, 'prince':3, 'seer':7, 'spellcaster':1, 'tough_guy':3, 'lycan':-1, 'mason':2, 'old_hag':1, 'mayor':2, 'troublemaker':-3,
-#                       'village_idiot':2, 'apprentice_seer':4, 'aura_seer':3, 'bodyguard':3, 'cult_leader':1, 'cupid':-3, 'diseased':3, 'doppelganger':-2, 'drunk':4, 'ghost':2, 'hunter':3 },
-#                'bad':{'werewolf':-6, 'wolf_cub':-8, 'sorceress':-3, 'tanner':-2, 'vampire':-7, 'cursed':-3, 'lone_wolf':-5, 'minion':-6 }
-#                }
-
 cards = [
         {'name':'villager',         'team':'good',  'weight':1,     'limit':12,     'des':''},
         {'name':'witch',            'team':'good',  'weight':4,     'limit':1,      'des':''},
@@ -48,12 +43,6 @@ cards = [
         ]
 
 
-
-#card_limits = {'villager':12, 'witch':1, 'pacifist':1, 'p.i':0, 'prince':0, 'seer':1, 'spellcaster':0, 'tough_guy':1, 'lycan':1, 'mason':2, 'old_hag':1, 'mayor':1, 'troublemaker':0,
-#                       'village_idiot':1, 'apprentice_seer':1, 'aura_seer':0, 'bodyguard':1, 'cult_leader':1, 'cupid':1, 'diseased':1, 'doppelganger':1, 'drunk':1, 'ghost':0, 'hunter':1,
-#                'werewolf':8, 'wolf_cub':1, 'sorceress':1, 'tanner':0, 'vampire':0, 'cursed':1, 'lone_wolf':1, 'minion':1
-#                }
-
 card_inclusions = {'villager':[['None']], 'witch':[['None']], 'pacifist':[['None']], 'p.i':[[]], 'prince':[[]], 'seer':[[]], 'spellcaster':[[]], 'tough_guy':[[]], 'lycan':[[]], 'mason':[['mason']], 'old_hag':[[]], 'mayor':[['villager']], 'troublemaker':[[]],
                        'village_idiot':[['None']], 'apprentice_seer':[['seer'],['aura_seer']], 'aura_seer':[[]], 'bodyguard':[[]], 'cult_leader':[[]], 'cupid':[[]], 'diseased':[[]], 'doppelganger':[[]], 'drunk':[[]], 'ghost':[[]], 'hunter':[[]],
                 'werewolf':[[]], 'wolf_cub':[['werewolf'],['lone_wolf']], 'sorceress':[['seer']], 'tanner':[[]], 'vampire':[[]], 'cursed':[[]], 'lone_wolf':[['werewolf']], 'minion':[[]]
@@ -63,10 +52,6 @@ card_exclusions = {'villager':[], 'witch':[], 'pacifist':['village_idiot'], 'p.i
                        'village_idiot':['pacifist'], 'apprentice_seer':['aura_seer', 'p.i','sorceress'], 'aura_seer':['seer','apprentice_seer','p.i'], 'bodyguard':[], 'cult_leader':['mayor','prince'], 'cupid':[], 'diseased':['vampire'], 'doppelganger':[], 'drunk':[], 'ghost':[], 'hunter':[],
                 'werewolf':['vampire'], 'wolf_cub':['vampire'], 'sorceress':['aura_seer'], 'tanner':[], 'vampire':['werewolf','wolf_cub','cursed','diseased','minion'], 'cursed':['vampire'], 'lone_wolf':[], 'minion':['vampire']
                 }
-
-# n_people = 10 # variable, number of players, or cards in the deck
-# target = 0 # variable, the target point value of the returned deck
-# threshold = 1 # variable, the 'margin' of freedom for returned deck values
 
 @app.route('/deck/<int:n_people>/<int:target>/<int:threshold>/<string:forced_roles>/<string:black_listed>')
 def card_selection(n_people, target, threshold, forced_roles, black_listed): # argv takes optional string arguments for specified cards we want
